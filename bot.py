75ab9ad52c1c77c392bf92fe438bb394d13578f3
Kvshcoder <33620645+Kvshcoder@users.noreply.github.com>
2018-06-14 20:13:42 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -101,5 +101,12 @@ def findUwU(message):
 	if uwu_words.search(message.text):
 		uwu_data = "*UwU*"
 		bot.send_message(message.chat.id, uwu_data,parse_mode='Markdown')
-	
+		
+@bot.message_handler(func=lambda message: True)
+def findOwO(message):
+	print("find OwO triggered!")
+	owo_words= re.compile('owo',re.IGNORECASE)
+	if owo_words.search(message.text):
+		owo_data = "*OwO*"
+		bot.send_message(message.chat.id, owo_data,parse_mode='Markdown')
 bot.polling()
