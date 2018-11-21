c220da39028a8d88b1442a3786e736f2f6158c80
Kvshcoder <33620645+Kvshcoder@users.noreply.github.com>
2018-11-21 22:00:30 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -33,8 +33,8 @@ def todb(message):
 	try:
 		conn = psycopg2.connect(DATABASE_URL, sslmode='require')
 		cur = conn.cursor()
-		query = """INSERT INTO msg(chat_id,message) VALUES (%(int)s, %s);"""
-		data = (chat_ido,msg_txto)
+		query = """INSERT INTO msg (chat_id,message) VALUES (%(int)s, %s);"""
+		data = [chat_ido, msg_txto]
 		cur.execute(query,data)
 		conn.commit()
 	except (Exception, psycopg2.Error) as error:
