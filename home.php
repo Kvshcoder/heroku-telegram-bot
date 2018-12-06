e4e93ccf37aff9d66d42ac8ba37a15321ffab8d5
kvsh <41984738+eprintms@users.noreply.github.com>
2018-12-06 19:56:54 +0530

    Update home.php

--- home.php
+++ home.php
@@ -16,7 +16,7 @@ echo "
 
 </html>";
 
-$conn = pg_connect(" postgres://kllwqbimfzujcz:e5996fb5d16c7abceb6cb6c922a817019b15e6ccf09851241330777a7a4d7bb4@ec2-79-125-12-48.eu-west-1.compute.amazonaws.com:5432/da5pslrps1jns5");
+$conn = "dbname=da5pslrps1jns5 host=ec2-79-125-12-48.eu-west-1.compute.amazonaws.com port=5432 user=kllwqbimfzujcz password=e5996fb5d16c7abceb6cb6c922a817019b15e6ccf09851241330777a7a4d7bb4 sslmode=require";
 $result = pg_query($conn, "SELECT * FROM msg");
 while ($row = pg_fetch_row($result)) {
     echo "<p>" . htmlspecialchars($row[0]) . "</p>\n";
