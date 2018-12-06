bcff5c33858684dd9516e093d30c136deeec9a8c
kvsh <41984738+eprintms@users.noreply.github.com>
2018-12-06 20:21:39 +0530

    Update home.php

--- home.php
+++ home.php
@@ -20,9 +20,8 @@ $conn = pg_connect("dbname=da5pslrps1jns5 host=ec2-79-125-12-48.eu-west-1.comput
 $result = pg_query($conn, "SELECT * FROM msg");
 while ($row = pg_fetch_row($result)) {
   echo "<p>" . htmlspecialchars($row[0]) . "</p>\n";
-  $result2 = pg_query($conn, "SELECT". $row[0] ."FROM msg");
+  $result2 = pg_query($conn, "SELECT ". $row[0] ." FROM msg");
   while ($column = pg_fetch_all($result2)) {
-    
     echo "<p>" . htmlspecialchars($column[0]) . "</p>\n";
   }
 
