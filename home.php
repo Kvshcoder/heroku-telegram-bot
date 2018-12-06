05a6d16a8e4edbf3081cfc68462336bdfbf328ae
kvsh <41984738+eprintms@users.noreply.github.com>
2018-12-06 19:30:50 +0530

    db connect

--- home.php
+++ home.php
@@ -9,9 +9,24 @@ echo "
     <h1> HEROKU PHP DEPLOY </h1>
 
       <b> UwU </b>
+      <br/>
       <b> OwO </b>
 
   </body>
 
-</html>"; 
+</html>";
+
+  $con = "dbname=da5pslrps1jns5 host=ec2-79-125-12-48.eu-west-1.compute.amazonaws.com port=5432 user=kllwqbimfzujcz password=e5996fb5d16c7abceb6cb6c922a817019b15e6ccf09851241330777a7a4d7bb4 sslmode=require";
+
+
+  if (!$con)
+  {
+    echo "Database connection failed.";
+  }
+  else
+  {
+    echo "Database connection success.";
+  }
+
+
 ?>
