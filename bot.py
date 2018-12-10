f55e8d64455ae48c7deeef6e13d8bf5fba9e2ff6
kvsh <41984738+eprintms@users.noreply.github.com>
2018-12-10 13:40:35 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -25,6 +25,17 @@ Buckets_list=cos_client.list_buckets()
 print(Buckets_list)
 object_list=cos_client.list_objects(Bucket="kvsh")
 print(object_list)
+def get_bucket_contents(bucket_name):
+    print("Retrieving bucket contents from: {0}".format(bucket_name))
+    try:
+        files = cos.Bucket(bucket_name).objects.all()
+        for file in files:
+            print("Item: {0} ({1} bytes).".format(file.key, file.size))
+    except ClientError as be:
+        print("CLIENT ERROR: {0}\n".format(be))
+    except Exception as e:
+        print("Unable to retrieve bucket contents: {0}".format(e))
+get_bucket_contents(bucket_name="kvsh")
 # Example of your code beginning
 #           Config vars
 token = os.environ['token']
