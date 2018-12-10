74ebcfa3440528830419eb041507839fd77a843f
kvsh <41984738+eprintms@users.noreply.github.com>
2018-12-10 16:05:32 +0530

    seperated fmibms3

--- bot.py
+++ bot.py
@@ -6,8 +6,7 @@ import re
 import psycopg2
 import sys
 from datetime import datetime
-from ibm_botocore.client import Config
-import ibm_boto3
+import fmibms3
 # import some_api_lib
 # import ...
 
@@ -16,38 +15,15 @@ import ibm_boto3
 # Example of your code beginning
 
 #           Config vars
-token = os.environ['token']
-tgadmin = os.environ['adminkey']
-DATABASE_URL = os.environ['DATABASE_URL']
-api_key = os.environ['cos_api_key']
-service_instance_id = os.environ['cos_resource_instance_id']
-auth_endpoint = 'https://iam.bluemix.net/oidc/token'
-service_endpoint = 'https://s3.us-east.objectstorage.softlayer.net'
+
 #some_api_token = os.environ['SOME_API_TOKEN']
 #             ...
 
 # If you use redis, install this add-on https://elements.heroku.com/addons/heroku-redis
 #r = redis.from_url(os.environ.get("REDIS_URL"))
-cos = ibm_boto3.resource('s3',
-                      ibm_api_key_id=api_key,
-                      ibm_service_instance_id=service_instance_id,
-                      ibm_auth_endpoint=auth_endpoint,
-                      config=Config(signature_version='oauth'),
-                      endpoint_url=service_endpoint)
-#       Your bot code below
-bot = telebot.TeleBot(token)
-# some_api = some_api_lib.connect(some_api_token)
-def get_item(bucket_name, item_name):
-    print("Retrieving item from bucket: {0}, key: {1}".format(bucket_name, item_name))
-    try:
-        file = cos.Object(bucket_name, item_name).get()
-        print("File Contents: {0}".format(file["Body"].read()))
-    except ClientError as be:
-        print("CLIENT ERROR: {0}\n".format(be))
-    except Exception as e:
-        print("Unable to retrieve file contents: {0}".format(e))
+
 #              ...
-get_item("kvsh","code2.txt")
+fmibms3.get_item("kvsh","code2.txt")
 # -------------------------
 conn = None
 def todbtext(message):
---
+++ fmibms3.py
@@ -0,0 +1,27 @@
+from ibm_botocore.client import Config
+import ibm_boto3
+token = os.environ['token']
+tgadmin = os.environ['adminkey']
+DATABASE_URL = os.environ['DATABASE_URL']
+api_key = os.environ['cos_api_key']
+service_instance_id = os.environ['cos_resource_instance_id']
+auth_endpoint = 'https://iam.bluemix.net/oidc/token'
+service_endpoint = 'https://s3.us-east.objectstorage.softlayer.net'
+cos = ibm_boto3.resource('s3',
+                      ibm_api_key_id=api_key,
+                      ibm_service_instance_id=service_instance_id,
+                      ibm_auth_endpoint=auth_endpoint,
+                      config=Config(signature_version='oauth'),
+                      endpoint_url=service_endpoint)
+#       Your bot code below
+bot = telebot.TeleBot(token)
+# some_api = some_api_lib.connect(some_api_token)
+def get_item(bucket_name, item_name):
+    print("Retrieving item from bucket: {0}, key: {1}".format(bucket_name, item_name))
+    try:
+        file = cos.Object(bucket_name, item_name).get()
+        print("File Contents: {0}".format(file["Body"].read()))
+    except ClientError as be:
+        print("CLIENT ERROR: {0}\n".format(be))
+    except Exception as e:
+        print("Unable to retrieve file contents: {0}".format(e))
