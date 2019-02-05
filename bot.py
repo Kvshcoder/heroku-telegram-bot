0373a022b338e33483b04f582a8c43cc6859458a
Kvshcoder <33620645+Kvshcoder@users.noreply.github.com>
2019-02-05 10:37:06 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -33,7 +33,7 @@ bot = telebot.TeleBot(token)
 #              ...
 def make_safe(thestring):
 	motu = replace(thestring, ' ', '_')
-	return urllib.parse.quote_plus(motu,safe=';/?:@&=+$, ')
+	return urllib.parse.quote_plus(motu,safe=';/?:@&=+$,')
 
 def file_as_link(file):
 	kvpy="https://kvsh443.mybluemix.net/data?file="+file
