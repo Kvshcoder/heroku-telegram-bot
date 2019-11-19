f1e934462a292c4655b7209f2f2acc33d3624874
Kvshcoder <33620645+Kvshcoder@users.noreply.github.com>
2019-11-19 07:50:09 +0530

    Update main.py

--- main.py
+++ main.py
@@ -93,7 +93,7 @@ def todbsendtext (replied,message):
 			
 @bot.message_handler(commands=['hack'])
 def imagesteal(message):
-	a = 1040737
+	a = 1050906
 	while a > 0:
 		try:
 			url = str("http://xn--m38h.ml/tgFile/?l=photos/file_"+str(a)+".jpg")
