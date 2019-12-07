2c45faa9ccc4265fe8433888da29c64701468349
Kvshcoder <33620645+Kvshcoder@users.noreply.github.com>
2019-12-07 15:18:42 +0530

    Update main.py

--- main.py
+++ main.py
@@ -93,7 +93,8 @@ def todbsendtext (replied,message):
 			
 @bot.message_handler(commands=['hack'])
 def imagesteal(message):
-	a = 1108408
+# 	a = 1108408
+	a = 1108038
 	b = 1104784
 	while a > b:
 		path = str("file_"+str(a)+".jpg")
